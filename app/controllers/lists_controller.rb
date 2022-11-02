48c582856326753fb12a20996abaefa6cd45e29c
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-02 15:05:01 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,16 @@
 class ListsController < ApplicationController
   def new
+    #viewへ渡すインスタンス変数に空のmodelオブジェクトを作る
+    @list=List.new
   end
-
+  def create
+    #1&2 データを受け取って新規登録するためのインスタンス作成
+    list=List.new(list_params)
+    #3 データをデータベースに保存するためのsaveメソッドの実行
+    list.save
+    #4
+    redirect_to '/top'
+  end
   def index
   end
 
@@ -9,5 +18,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end
