cb38e8abf2092ba56f644c1f7afb922322c4108b
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-04 11:03:44 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,9 +3,12 @@ class ListsController < ApplicationController
     @list = List.new
   end
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id) 
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
   def index
     @lists = List.all
@@ -23,6 +26,12 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
   private
